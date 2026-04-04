a52656855ea0fae016f69d215fd76afdbcc8c6d8
Ruslan <apanaevrus@gmail.com>
2026-04-04 22:23:53 +0400

    внёс правки 04.04.26 v2

--- test_login.py
+++ test_login.py
@@ -2,16 +2,15 @@ from playwright.sync_api import expect
 from faker import Faker
 
 fake = Faker()
-
+URL = "http://144.31.139.115:5000"
 
 def test_input_login_and_password(page):
-    url = "http://144.31.139.115:5000"
-    page.goto(url)
+    page.goto(URL)
     login_link = page.get_by_test_id("nav-login")
     login_link.click()
-    page.locator("//input[@id='username']").fill(fake.user_name())
-    page.locator("//input[@id='password']").fill(fake.password())
-    page.locator("//input[@id='password']").press("Enter")
+    page.locator("//*[@id='username']").fill(fake.user_name())
+    page.locator("//*[@id='password']").fill(fake.password())
+    page.locator("//*[@id='password']").press("Enter")
 
     loading_indicator = page.get_by_test_id("login-submixt-spinner")
     expect(loading_indicator).to_be_visible()
